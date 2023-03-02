c4a80844b57ab960163c0cbd4bda771f74b2a937
nibeditans <126712682+nibeditans@users.noreply.github.com>
2023-03-02 15:15:22 +0530

    Add files via upload
    "Number_Guessing_Game" is a Python program that challenges players to guess a randomly generated number within a set range. With a simple user interface and easy-to-understand rules, this game provides hours of entertainment for players of all ages. The code is well-documented and easy to follow, making it a great resource for anyone learning Python. Download the program, give it a try, and see if you can guess the number before the game ends!

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,39 @@
+import random
+
+play_again = 'yes'
+
+while play_again == 'yes':
+    # generate a random number between 1 and 100
+    number = random.randint(1, 100)
+
+    # set the number of guesses allowed to 10
+    num_guesses_allowed = 10
+
+    # ask the user to guess the number
+    print("Guess the number between 1 and 100.")
+    print("Before playing remember than you have only", num_guesses_allowed, "guesses. \nPlay well Dude:)")
+    guess = int(input("Enter your guess: "))
+
+    # keep track of the number of guesses
+    num_guesses = 1
+
+    # check if the user's guess is correct
+    while guess != number and num_guesses < num_guesses_allowed:
+        if guess < number:
+            print("Too low! \nNow you have only", num_guesses_allowed - num_guesses, "guesses left.")
+        else:
+            print("Too high! \nNow you have only", num_guesses_allowed - num_guesses, "guesses left.")
+        guess = int(input("Enter your guess: "))
+        num_guesses += 1
+
+    # if the user's guess is correct, congratulate them and tell them how many guesses it took
+    if guess == number:
+        print("Congratulations, you guessed the number in", num_guesses, "guesses!")
+    else:
+        print("\nGame over:( \nOops! You couldn't guess the number in",
+              num_guesses_allowed, "guesses. \nBy the way the number was", number, ".")
+
+    # ask the user if they want to play again
+    play_again = input("Do you want to play again? Enter 'yes' or 'no': ").lower()
+
+print("Okay! Thanks for playing!")
